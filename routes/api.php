7e012a800648f2824ee2a778a55e81d129c77701
dandy.yusuf <dandy.yusuf@codr.id>
2025-10-24 14:33:07 +0700

    Fix route

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Backward-compatible routes
 Route::post('/user', [UserController::class, 'store']);
 Route::get('/user', [UserController::class, 'index']);
-
-// Preferred pluralized endpoints
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users', [UserController::class, 'index']);
